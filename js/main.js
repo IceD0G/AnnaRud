a7f6f4739840040a8dc5e07b63c3891b747c329c
IceD0G <34693762+IceD0G@users.noreply.github.com>
2020-09-02 16:15:51 +0300

    end

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 const header = document.querySelectorAll('header')
-console.log(header)
+const links = document.querySelectorAll('.header-logo>a,.header-info>ul>li>a')
+window.addEventListener('scroll',()=>{
+    if (window.scrollY >= 10) {
+        header[0].style.background = '#fff'    
+        header[0].style.padding = '1em 0'  
+        for (let index = 0; index < links.length; index++) {
+            links[index].style.color = '#000'
+        }  
+    } else{
+        header[0].style.background = 'transparent' 
+        header[0].style.padding = '2em 0'
+        for (let index = 0; index < links.length; index++) {
+            links[index].style.color = '#fff'
+        }  
+    } 
+})
